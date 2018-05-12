3c49420dd26810f464a885595fec1394b57997a9
AntonHermann <antonoehler@gmx.de>
2018-05-12 22:56:05 +0200

    code cleanup

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -12,35 +12,18 @@ use self::systems::*;
 pub struct GameActive(bool);
 
 pub fn run_game() -> Result<(), Error> {
-    // let mut stdout = stdout().into_raw_mode()?;
-    let mut world = World::new();
-    world.register::<Position>();
-    world.register::<Appearance>();
-    world.add_resource(stdout().into_raw_mode()?);
-    world.add_resource(GameActive(true));
+    let mut world = init_world()?;
+    init_player(&mut world);
 
-    let _player = world
-        .create_entity()
-        .with(Position { x: 0, y: 2 })
-        .with(Appearance::Player)
-        .build();
-
-    // let mut rendering_system = RenderingSystem;
-    // let mut player_interaction_system = PlayerInteractionSystem;
     let mut dispatcher = DispatcherBuilder::new()
         .add(RenderingSystem, "rendering_system", &[])
         .add(PlayerInteractionSystem, "player_interaction_system", &[])
-        // .build_async(world);
         .build();
 
     let sleep_duration = time::Duration::from_millis(10);
 
-    // dispatcher.dispatch(&mut world.res);
-    // stdin().read_line(&mut String::new())?;
-
     loop {
         dispatcher.dispatch(&mut world.res);
-        // dispatcher.dispatch();
         if *(&world.read_resource::<GameActive>().0) == false {
             break;
         }
@@ -48,4 +31,20 @@ pub fn run_game() -> Result<(), Error> {
     }
 
     Ok(())
+}
+
+fn init_world() -> Result<World, Error> {
+    let mut world = World::new();
+    world.register::<Position>();
+    world.register::<Appearance>();
+    world.add_resource(stdout().into_raw_mode()?);
+    world.add_resource(GameActive(true));
+    Ok(world)
+}
+
+fn init_player(world: &mut World) {
+    world.create_entity()
+        .with(Position { x: 0, y: 2 })
+        .with(Appearance::Player)
+        .build();
 }
