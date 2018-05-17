7b18e4077f752604ab59b6655460fba9c229d88b
AntonHermann <antonoehler@gmx.de>
2018-05-17 19:51:49 +0200

    cleanup on display system

--- src/game/display.rs
+++ src/game/display.rs
@@ -2,45 +2,25 @@ use std::io::{Stdout, stdout, Write};
 use termion;
 use termion::{clear, cursor};
 use termion::raw::{RawTerminal, IntoRawMode};
-// use specs::{System, ReadStorage, FetchMut, Join};
 use specs::prelude::*;
 
 use super::components::*;
 
-pub struct Terminal(RawTerminal<Stdout>);
-impl Default for Terminal {
-    fn default() -> Self {
-        Terminal(stdout().into_raw_mode().expect("couldn't get stdout"))
-    }
+fn flush(rt: &mut RawTerminal<Stdout>) {
+    rt.flush().expect("failed flushing stdout");
 }
-impl ::std::ops::Deref for Terminal {
-    type Target = RawTerminal<Stdout>;
-    fn deref(&self) -> &RawTerminal<Stdout> {
-        &self.0
-    }
-}
-impl ::std::ops::DerefMut for Terminal {
-    fn deref_mut(&mut self) -> &mut RawTerminal<Stdout> {
-        &mut self.0
-    }
-}
-impl Terminal {
-    pub fn flush(&mut self) {
-        self.0.flush().expect("failed flushing stdout");
-    }
-    pub fn clear_screen(&mut self) {
-        write!(self.0, "{}{}", termion::clear::All, termion::cursor::Hide)
-            .expect("couldn't write to stdout");
-    }
+fn clear_screen(rt: &mut RawTerminal<Stdout>) {
+    write!(rt, "{}{}", termion::clear::All, termion::cursor::Hide)
+        .expect("couldn't write to stdout");
 }
 
 pub struct RenderingSystem {
-    stdout: Terminal,
+    stdout: RawTerminal<Stdout>,
 }
 impl RenderingSystem {
     pub fn new() -> Self {
-        let mut stdout = Terminal::default();
-        stdout.clear_screen();
+        let mut stdout = stdout().into_raw_mode().expect("couldn't get stdout");
+        clear_screen(&mut stdout);
 
         RenderingSystem {
             stdout
@@ -58,7 +38,8 @@ impl<'a> System<'a> for RenderingSystem {
         // TODO: add error handling
         let (_, term_height) = termion::terminal_size().expect("couldn't get terminal size");
 
-        write!(self.stdout, "{}", clear::All).expect("couldn't clear screen");
+        // write!(self.stdout, "{}", clear::All).expect("couldn't clear screen");
+        clear_screen(&mut self.stdout);
         for (p, a) in (&pos, &ap).join() {
             trace!("rendering {:?} {:?}", a, p);
             let y = term_height - p.y as u16;
@@ -71,6 +52,6 @@ impl<'a> System<'a> for RenderingSystem {
                 a.to_string()
             ).expect("couldn't print to stdout");
         }
-        self.stdout.flush();
+        flush(&mut self.stdout);
     }
 }
